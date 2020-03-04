228ebc3617784b50f86e005ff8212c981cf5fe3f
Jorge Suárez Burgos <baloncesto-realejos-6@outlook.es>
2020-03-04 09:27:11 +0000

    bot

--- U5/a2/bot15d.rb
+++ U5/a2/bot15d.rb
@@ -2,7 +2,7 @@
 
 require 'telegram/bot'
 
-token = `cat hide.token`.strip
+token = %x[cat /etc/bot15/token].strip
 bot_username = '@jsuabur_bot'
 
 puts "[INFO] Running bot #{$0}..."
@@ -21,22 +21,10 @@ Telegram::Bot::Client.run(token) do |bot|
       /help -> Ver la ayuda
       /hello -> Saludar
       /bye -> Despedirse
-      /chatid -> Ver ID del grupo
-      /lottery -> Seleccionar un número"
+      /number -> Teclado numérico"
       bot.api.send_message(chat_id: message.chat.id, text: say)
-    elsif options[0] == "/location"
-      location =
-      Telegram::Bot::Types::ReplyKeyboardMarkup.new(
-      keyboard: [
-        ["Localizacion actual"]
-      ],
-      request_location: true
-      )
-      bot.api.send_message(chat_id: message.chat.id, text: "hola", reply_markup: location)
-    elsif options[0] == "/chatid"
-      bot.api.send_message(chat_id: message.chat.id, text: "Id is: #{message.chat.id}")
-    elsif options[0] == "/lottery"
-      question = "choose a number:"
+    elsif options[0] == '/number'
+      question = 'Seleccionar un número:'
       answers =
       Telegram::Bot::Types::ReplyKeyboardMarkup.new(
       keyboard: [
@@ -48,9 +36,6 @@ Telegram::Bot::Client.run(token) do |bot|
       one_time_keyboard: true
       )
       bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
-      if options[0] == 1
-        bot.api.send_message(chat_id: message.chat.id, text: "Seleccionaste el 1")
-      end
     end
   end
 end
